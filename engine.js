224ac15bff3b9609b79ec30f261976c7dab79924
realrentao <113217858@qq.com>
2026-09-12 10:43:07 +0800

    更新 VCONJ 变位引擎元数据

--- engine.js
+++ engine.js
@@ -202,6 +202,7 @@
             "cosatare":   {type:"are", stem:"cosat", aux:"avere", pp:"cosatato"},
             // D
 
+    "dimenticarsi": {type:"are-rfl", stem:"dimentic", aux:"essere", pp:"dimenticato"},
     "dirige":     {type:"ere", stem:"dirig", aux:"avere", pp:"diretto", pres:["dirigo","dirigi","dirige","dirigiamo","dirigete","dirigono"]},            "dare":       {type:"irr"},
             "decidere":   {type:"ere", stem:"decid", aux:"avere", pp:"deciso", rem:["decisi","decidesti","decise","decidemmo","decideste","decisero"]},
             "depilarsi":  {type:"are-rfl", stem:"depil", aux:"essere", pp:"depilato"},
